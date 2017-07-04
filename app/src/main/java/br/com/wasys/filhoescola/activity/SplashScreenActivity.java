56b459edf4245b34cb47d9bc851860fed353c397
Bruno Scrok Brunoro <bruno.sbrunoro@gmail.com>
2017-07-03 21:26:57 -0300

    tela de splash screen

---
+++ app/src/main/java/br/com/wasys/filhoescola/activity/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package br.com.wasys.filhoescola.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import br.com.wasys.filhoescola.R;
+import br.com.wasys.library.activity.AppActivity;
+
+public class SplashScreenActivity extends AppActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        startActivity(new Intent(this,CadastroActivity.class));
+
+    }
+
+}
